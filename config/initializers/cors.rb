d544d1adff8dba7af32254c75d176132e73a1e6b
Harkavenko <garkavenkodenispost@gmail.com>
2024-03-13 01:48:25 +0100

    complete basic configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # change this to the desired address
-    origin 'http://localhost:5137'
+    origins 'http://localhost:5137'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
